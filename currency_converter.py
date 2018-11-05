ecef87216c0b98a6f2a8ecf12e245def3339d130
Mo Glatz <meagabeth@icloud.com>
2018-11-05 15:18:33 -0500

    Function now converts values, 1 & other

--- currency_converter.py
+++ currency_converter.py
@@ -1,6 +1,12 @@
+# list = [(value, form, to)]
+rates = [
+    ("USD", "EUR", 0.74),
+    ("EUR", "JPY", 145.949)
+    ]
 
-rates = []
-
-def convert(list, value, form, to):
-    if form == to:
+def convert(list, value, original, to):
+    for conversion in rates:
+        if original == conversion[0] and to == conversion[1]:
+            if original == conversion[0]:
+                return value * conversion[2]
         return value
--- test_currency_converter.py
+++ test_currency_converter.py
@@ -1,5 +1,11 @@
 from currency_converter import convert
 
+rates = [
+    ("USD", "EUR", 0.74),
+    ("EUR", "JPY", 145.949)
+    ]
+
 def test_convert():
-    rates = []
-    assert convert(rates, 12, form = "e", to = "e") == 12
+    assert convert(rates, 1, "USD", "EUR") == 0.74
+    assert convert(rates, 10, "USD", "EUR") == 7.4
+
