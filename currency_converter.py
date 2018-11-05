42975b93ef878195820e708cc0552836abfadcce
Mo Glatz <meagabeth@icloud.com>
2018-11-05 16:20:28 -0500

    We have new conversion rates, can convert backwards and forwards

--- currency_converter.py
+++ currency_converter.py
@@ -5,8 +5,10 @@ rates = [
     ]
 
 def convert(list, value, original, to):
+    if original == to:
+        return value
     for conversion in rates:
         if original == conversion[0] and to == conversion[1]:
-            if original == conversion[0]:
-                return value * conversion[2]
-        return value
+            return value * conversion[2]
+        if original == conversion[1] and to == conversion[0]:
+            return value / conversion[2]    
--- test_currency_converter.py
+++ test_currency_converter.py
@@ -3,9 +3,14 @@ from currency_converter import convert
 rates = [
     ("USD", "EUR", 0.74),
     ("EUR", "JPY", 145.949)
+    
     ]
 
 def test_convert():
     assert convert(rates, 1, "USD", "EUR") == 0.74
     assert convert(rates, 10, "USD", "EUR") == 7.4
+    assert convert(rates, 0.74, "EUR", "USD") == 1
+    assert convert(rates, 1, "EUR", "EUR") == 1
+    assert round(convert(rates, 1, "EUR", "JPY"), 2) == 145.95
+
 
